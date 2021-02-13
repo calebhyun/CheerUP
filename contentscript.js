75e0e187829aa2bc917e115c17c9bc24a6952ea5
Caleb Hyun <calebhyun@live.com>
2021-02-13 10:54:09 -0800

    Initial commit

---
+++ contentscript.js
@@ -0,0 +1,26 @@
+
+/*deepai.setApiKey('bc446df1-89bc-4b99-8cac-02b1a4e95a01');
+
+(async function() {
+    var resp = await deepai.callStandardApi("sentiment-analysis", {
+            text: "HI! This is a positive sentence.",
+    });
+    console.log(resp);
+    alert(resp)
+})()*/
+
+
+
+// Example posting file picker input text (Browser only):
+
+/*const deepai = require('deepai'); // OR include deepai.min.js as a script tag in your HTML
+
+deepai.setApiKey('bc446df1-89bc-4b99-8cac-02b1a4e95a01');
+
+(async function() {
+    var resp = await deepai.callStandardApi("sentiment-analysis", {
+            text: document.getElementById('yourFileInputId'),
+    });
+    console.log(resp);
+})()
+*/
---
+++ popup.js
@@ -0,0 +1,36 @@
+deepai.setApiKey('bc446df1-89bc-4b99-8cac-02b1a4e95a01'); 
+
+function checkSentiment(){
+
+        var strText = $('#myText').val();
+
+        deepai.callStandardApi("sentiment-analysis", {
+
+            text: strText
+
+        }).then(function(r) {
+            alert(r.output[0])
+
+            if (r.output[0] =="Negative"){
+
+                $.get( "https://dog.ceo/api/breeds/image/random", function( data ) {
+
+                    $('#divPic').prepend('<img id="theImg" src="' + data.message + '" width="200px" />')
+
+                });
+
+            }
+
+        }, function(err) {
+
+        alert(err); // Error: "It broke"
+
+        });
+
+    }
+
+    $('#btnSubmit').click(function() {
+
+        checkSentiment();
+    
+    });
